ab065fd0dbb92a66db2b9fd59b2cbffa803ab1ba
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2026-06-24 14:25:19 +0300

    refactor: clean up code formatting and structure

--- www/cgi-bin/chat.py
+++ www/cgi-bin/chat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import os
 import sys
 import json
@@ -11,90 +10,82 @@ MAX_MESSAGES = 200
 
 write = sys.stdout.buffer.write
 
-
 def load_messages():
-    try:
-        with open(CHAT_FILE, encoding="utf-8") as f:
-            return json.load(f)
-    except (OSError, ValueError):
-        return []
+	try:
+		with open(CHAT_FILE, encoding="utf-8") as f:
+			return json.load(f)
+	except (OSError, ValueError):
+		return []
 
 
 def save_messages(messages):
-    with open(CHAT_FILE, "w", encoding="utf-8") as f:
-        json.dump(messages[-MAX_MESSAGES:], f, ensure_ascii=False)
-
+	with open(CHAT_FILE, "w", encoding="utf-8") as f:
+		json.dump(messages[-MAX_MESSAGES:], f, ensure_ascii=False)
 
 def parse_post_body():
-    try:
-        length = int(os.environ.get("CONTENT_LENGTH") or 0)
-    except ValueError:
-        length = 0
-    if length <= 0:
-        return {}
-    body = sys.stdin.buffer.read(length).decode("utf-8", "replace")
-    fields = {}
-    for pair in body.split("&"):
-        if "=" in pair:
-            key, value = pair.split("=", 1)
-            fields[key] = unquote_plus(value)
-    return fields
-
+	try:
+		length = int(os.environ.get("CONTENT_LENGTH") or 0)
+	except ValueError:
+		length = 0
+	if length <= 0:
+		return {}
+	body = sys.stdin.buffer.read(length).decode("utf-8", "replace")
+	fields = {}
+	for pair in body.split("&"):
+		if "=" in pair:
+			key, value = pair.split("=", 1)
+			fields[key] = unquote_plus(value)
+	return fields
 
 def send_header(content_type):
-    write(("Content-Type: %s; charset=utf-8\r\n\r\n" % content_type).encode("utf-8"))
-
+	write(("Content-Type: %s; charset=utf-8\r\n\r\n" % content_type).encode("utf-8"))
 
 def send_json(messages):
-    send_header("application/json")
-    write(json.dumps(messages, ensure_ascii=False).encode("utf-8"))
-
+	send_header("application/json")
+	write(json.dumps(messages, ensure_ascii=False).encode("utf-8"))
 
 def handle_post():
-    fields = parse_post_body()
-    nick = fields.get("nick", "").strip()[:24]
-    text = fields.get("text", "").strip()[:500]
-
-    messages = load_messages()
-    if nick and text:
-        messages.append({
-            "nick": nick,
-            "text": text,
-            "time": time.strftime("%H:%M:%S"),
-        })
-        save_messages(messages)
-        messages = load_messages()
-    send_json(messages)
-
+	fields = parse_post_body()
+	nick = fields.get("nick", "").strip()[:24]
+	text = fields.get("text", "").strip()[:500]
+	messages = load_messages()
+	if nick and text:
+		messages.append({
+			"nick": nick,
+			"text": text,
+			"time": time.strftime("%H:%M:%S"),
+		})
+		save_messages(messages)
+		messages = load_messages()
+	send_json(messages)
 
 def handle_get():
-    if os.environ.get("QUERY_STRING"):
-        send_json(load_messages())
-    else:
-        send_header("text/html")
-        write(PAGE.encode("utf-8"))
-
+	if os.environ.get("QUERY_STRING"):
+		send_json(load_messages())
+	else:
+		send_header("text/html")
+		write(PAGE.encode("utf-8"))
 
 PAGE = """<!doctype html>
 <meta charset="utf-8">
 <title>chat</title>
 
-<input id="nick" placeholder="nick">
-<input id="text" placeholder="mesaj">
-<button onclick="send()">gonder</button>
+<input id="nick" placeholder="username">
+<input id="text" placeholder="message">
+<button onclick="send()">send</button>
 
-Yenileme:
+refresh:
 <select id="interval">
-  <option value="1">1 sn</option>
-  <option value="2">2 sn</option>
-  <option value="3">3 sn</option>
-  <option value="4">4 sn</option>
-  <option value="5">5 sn</option>
-  <option value="6">6 sn</option>
-  <option value="7">7 sn</option>
-  <option value="8">8 sn</option>
-  <option value="9">9 sn</option>
-  <option value="10">10 sn</option>
+  <option value="1">1 second</option>
+  <option value="2">2 second</option>
+  <option value="3">3 second</option>
+  <option value="4">4 second</option>
+  <option value="5">5 second</option>
+  <option value="6">6 second</option>
+  <option value="7">7 second</option>
+  <option value="8">8 second</option>
+  <option value="9">9 second</option>
+  <option value="10">10 second</option>
 </select>
 
 <div id="box"></div>
@@ -104,24 +95,24 @@ var timer = null;
 
 function load() {
   fetch("chat.py?messages")
-    .then(function (r) { return r.json(); })
-    .then(function (messages) {
-      box.innerHTML = messages.map(function (m) {
-        return m.time + " <b>" + m.nick + ":</b> " + m.text;
-      }).join("<br>");
-    });
+	.then(function (r) { return r.json(); })
+	.then(function (messages) {
+	  box.innerHTML = messages.map(function (m) {
+		return m.time + " <b>" + m.nick + ":</b> " + m.text;
+	  }).join("<br>");
+	});
 }
 
 function send() {
   var body = "nick=" + encodeURIComponent(nick.value) +
-             "&text=" + encodeURIComponent(text.value);
+			 "&text=" + encodeURIComponent(text.value);
   fetch("chat.py", { method: "POST", body: body })
-    .then(function (r) { return r.json(); })
-    .then(function (messages) {
-      box.innerHTML = messages.map(function (m) {
-        return m.time + " <b>" + m.nick + ":</b> " + m.text;
-      }).join("<br>");
-    });
+	.then(function (r) { return r.json(); })
+	.then(function (messages) {
+	  box.innerHTML = messages.map(function (m) {
+		return m.time + " <b>" + m.nick + ":</b> " + m.text;
+	  }).join("<br>");
+	});
   text.value = "";
 }
 
@@ -139,7 +130,7 @@ startPolling();
 """
 
 if __name__ == "__main__":
-    if os.environ.get("REQUEST_METHOD") == "POST":
-        handle_post()
-    else:
-        handle_get()
+	if os.environ.get("REQUEST_METHOD") == "POST":
+		handle_post()
+	else:
+		handle_get()
